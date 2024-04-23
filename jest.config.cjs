31feb32e3f80dd655b170aee1242781ec992e9ea
Rafael <raferdev@outlook.com>
2024-04-22 21:23:53 -0300

    test(root): update jest config to support typescript

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+// eslint-disable-next-line
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testEnvironmentOptions: {
+    NODE_ENV: "test",
+  },
+};
