bee67e1364aa6d9f8b7a9c8df8b43ce013a4e6af
Rafael <raferdev@outlook.com>
2024-04-23 02:36:42 -0300

    feat(services): add customers get id return data

--- src/services/customers/customers-get-id-service.ts
+++ src/services/customers/customers-get-id-service.ts
@@ -1,5 +1,7 @@
 import { customerGetByIdRepository } from "@/repositories/customers/customers-get-id-repository";
 
 export async function customersGetByIdService(id: string) {
-  return await customerGetByIdRepository(id);
+  const { Item } = await customerGetByIdRepository(id);
+
+  return Item;
 }
