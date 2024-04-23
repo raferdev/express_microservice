9f4d9830a949565f48868adf349a8844a1a82706
Rafael <raferdev@outlook.com>
2024-04-23 05:07:54 -0300

    test(units): add schema middleware first test

--- tests/unit/middlewares/schemas.test.ts
+++ tests/unit/middlewares/schemas.test.ts
@@ -1,7 +1,37 @@
-describe("test", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    const sum = (a: number, b: number) => a + b;
+import { customerSchemaMiddleware } from "@/middlewares/customers/customers-schema-middleware";
 
-    expect(sum(1, 2)).toBe(3);
+const mockReq = () => {
+  const req = {
+    body: {
+      name: "test",
+    },
+  };
+  // eslint-disable-next-line
+  return req as any;
+};
+
+const mockRes = () => {
+  // eslint-disable-next-line
+  const res = {} as any;
+
+  res.status = jest.fn().mockImplementation((code) => {
+    return {
+      json: jest.fn().mockImplementation((message) => {
+        return { status: code, message: message };
+      }),
+    };
+  });
+  return res;
+};
+
+describe("Middleware - Schema", () => {
+  test("Need to fail when customer body have only ", async () => {
+    const mockedNext = jest.fn();
+    const mockedReq = mockReq();
+    const mockedRes = mockRes();
+
+    await customerSchemaMiddleware(mockedReq, mockedRes, mockedNext);
+
+    expect(mockedRes.status).toHaveBeenCalledWith(400);
   });
 });
