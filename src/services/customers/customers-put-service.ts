5bbe875a9b32a15b81076184278853c033ca503f
Rafael <raferdev@outlook.com>
2024-04-23 02:38:59 -0300

    feat(services): add customers put service return data

--- src/services/customers/customers-put-service.ts
+++ src/services/customers/customers-put-service.ts
@@ -2,5 +2,7 @@ import { Customer } from "@/@types/database.js";
 import { customerPutRepository } from "@/repositories/customers/customers-put-repository";
 
 export async function customersPutService(customer: Customer) {
-  return await customerPutRepository(customer);
+  const { Attributes } = await customerPutRepository(customer);
+
+  return Attributes;
 }
