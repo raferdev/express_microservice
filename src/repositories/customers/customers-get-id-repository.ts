8631942e90cc2f2d0805527a77cbe86866557bdc
Rafael <raferdev@outlook.com>
2024-04-22 23:35:38 -0300

    feat(repositories): add get customer by id database connection

---
+++ src/repositories/customers/customers-get-id-repository.ts
@@ -0,0 +1,15 @@
+import { dynamodb } from "@/config/database";
+import { env } from "@/config/env";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+
+export async function customerGetByIdRepository(id: string) {
+  const customer = await dynamodb.send(
+    new GetCommand({
+      TableName: env.DYNAMODB_TABLE,
+      Key: {
+        id,
+      },
+    })
+  );
+  return customer;
+}
