3b840cc5f2339c3f1e58df021a4d3d3cafcd562f
Rafael <raferdev@outlook.com>
2024-04-23 02:28:36 -0300

    refactor(repositories): return database invocation directly

--- src/repositories/customers/customers-put-repository.ts
+++ src/repositories/customers/customers-put-repository.ts
@@ -5,7 +5,7 @@ import { v4 } from "uuid";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
 export async function customerPutRepository(customer: Customer) {
-  await dynamodb.send(
+  return await dynamodb.send(
     new PutCommand({
       TableName: env.DYNAMODB_TABLE,
       Item: {
@@ -14,5 +14,4 @@ export async function customerPutRepository(customer: Customer) {
       },
     })
   );
-  return customer;
 }
